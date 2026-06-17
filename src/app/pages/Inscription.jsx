ddc2710f3e8e9445ef5f25405e6dfda5ef4c60a8
moukjr10-bit <moukjr10@gmail.com>
2026-06-17 15:54:28 +0000

    commit

--- src/app/pages/Inscription.jsx
+++ src/app/pages/Inscription.jsx
@@ -1,6 +1,6 @@
 import React, { useState ,  } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
-
+   const URL_FRONT = import.meta.env.VITE_URL_FRONT;
 
 
 const Inscription = () => {
@@ -31,7 +31,7 @@ const Inscription = () => {
         };
 
         try {
-            const response = await fetch("http://localhost:3000/api/auth/inscription", {
+            const response = await fetch(`${URL_FRONT}/api/auth/inscription`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
